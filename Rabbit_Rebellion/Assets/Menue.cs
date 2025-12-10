d99156f94995e02cb79482a0d8feb69aac98d134
MIC4H-3kP0 <mae24a@acu.edu>
2025-12-09 23:06:02 -0600

    menue

---
+++ Rabbit_Rebellion/Assets/Menue.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Menue : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
